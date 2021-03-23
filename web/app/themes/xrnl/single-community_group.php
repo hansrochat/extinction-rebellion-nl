8b9d932d86ede0e5d6177513637a2d39c3b62028
franei <frank@yora.net>
2021-03-23 12:13:51 +0100

    Show full hero image on all display sizes

--- web/app/themes/xrnl/single-community_group.php
+++ web/app/themes/xrnl/single-community_group.php
@@ -6,29 +6,24 @@
 get_header(); ?>
 
 <?php
-$communityPage = apply_filters('wpml_object_id', 6832, 'page', true); // XRNL: 6832 NL, 6853 EN
-$communityPageURL = get_permalink($communityPage);
-$dark_image_overlay = get_field('group_cover_image_darkened');
-$hero_text_color = $dark_image_overlay ? 'white' : 'black';
+  $communityPage = apply_filters('wpml_object_id', 6832, 'page', true); // Page ID: 6832 NL, 6853 EN
+  $communityPageURL = get_permalink($communityPage);
 ?>
 
 <div class="community-group">
+
   <div class="hero-container">
-    <div class="bg-blue px-3 py-lg-5 pb-5 text-center text-<?php echo($hero_text_color) ?> group-cover-image" style="background: <?php if($dark_image_overlay):?> linear-gradient(rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.45)), <?php endif ?>url('<?php the_field('group_cover_image_url'); ?>'); no-repeat;">
-      <div class="py-5">
-        <h1 class="display-2 text-uppercase font-xr"><?php the_title(); ?></h1>
-      </div>
-    </div>
-    <div class="col-lg-8 mx-auto hero-caption text-<?php echo($hero_text_color); ?>">
+    <img class="group-cover-image mx-auto" src="<?php the_field('group_cover_image_url'); ?>" alt="<?php the_title(); ?>">
+    <div class="hero-caption">
       <?php the_field('group_image_caption'); ?>
     </div>
   </div>
 
   <div class="my-sm-5 my-4">
-    <div class="container py-5 text-center">
+    <div class="container pt-3 pb-5 text-center">
       <div class="row">
         <div class="col-12 col-lg-8 mx-auto">
-            <h1><?php the_title(); ?></h1>
+            <h1 class="display-4"><?php the_title(); ?></h1>
             <p class="text-justify"><?php the_content(); ?></p>
             <h2 class="group-slogan"><?php the_field('group_slogan'); ?></h2>
             <span class="group-social-icons">
