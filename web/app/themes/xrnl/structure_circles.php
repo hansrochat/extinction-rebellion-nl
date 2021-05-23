866efa55626c1343e1d1a0319bc04fa8e2a25614
Berry Groenendijk <berry.groenendijk@gmail.com>
2021-05-23 16:40:20 +0200

    Added a title to the introduction text. Added possibility to add 1 or more links. Changed the href of each circle to 'circle-[title circle]'. Added hints in custom fields to guide editors.

--- web/app/themes/xrnl/structure_circles.php
+++ web/app/themes/xrnl/structure_circles.php
@@ -14,35 +14,43 @@ get_header(); ?>
 <main class="structure-circles">
   <div class="container py-5">
     <div class="col-lg-12 mb-5 text-justify">
+        <header class="entry-header">
+                <h1 class="entry-title"><?php the_title(); ?></h1>
+        </header>
       <?php the_content(); ?>
     </div>
 
     <?php if( have_rows('circles') ): ?>
         <div class="col-lg-12 mb-5 text-justify">
+            <h1 class="font-xr" href="#working-groups"><?php the_field('title') ?></h1>
             <?php the_field('introduction') ?>
-        </div>
-        <div class="row">
-          <?php while ( have_rows('circles') ){ the_row();
-          $groupId = "circle-" . get_row_index();
-        ?>
-          <div class="col-12 py-2">
-            <div id="<?php echo formatElementID(get_sub_field('title')); ?>" class="mx-auto">
-              <a class="btn btn-yellow btn-lg btn-block text-left" data-toggle="collapse" href="#<?php echo $groupId; ?>"
-                role="button" aria-expanded="false" aria-controls="<?php echo $groupId; ?>">
-                <?php the_sub_field('title') ?>
-                <i class="fas fa-chevron-down float-right pt-1"></i>
-              </a>
-              <div class="text-left collapse pt-2" id="<?php echo $groupId; ?>">
-                <div class="pt-3 text-justify">
-                    <?php the_sub_field('description'); ?>
-                </div>
-                <div class="pt-3 text-justify">
-                    <a class="btn btn-lg btn-blue" href="<?php the_sub_field('link'); ?>">Wiki: <?php the_sub_field('title') ?></a>
+            <div class="row">
+              <?php while ( have_rows('circles') ){ the_row();
+              $groupId = "circle-" . formatElementID(get_sub_field('title'));
+            ?>
+              <div class="col-12 py-2">
+                <div id="<?php echo formatElementID(get_sub_field('title')); ?>" class="mx-auto">
+                  <a class="btn btn-yellow btn-lg btn-block text-left" data-toggle="collapse" href="#<?php echo $groupId; ?>"
+                    role="button" aria-expanded="false" aria-controls="<?php echo $groupId; ?>">
+                    <?php the_sub_field('title') ?>
+                    <i class="fas fa-chevron-down float-right pt-1"></i>
+                  </a>
+                  <div class="text-left collapse pt-2" id="<?php echo $groupId; ?>">
+                    <div class="pt-3 text-justify">
+                        <?php the_sub_field('description'); ?>
+                    </div>
+                    <div class="pt-3 text-justify">
+                        <?php if (have_rows('links')) : ?>
+                          <?php while ( have_rows('links') ){ the_row(); ?>
+                            <a class="btn btn-lg btn-black" href="<?php the_sub_field('link'); ?>"><?php the_sub_field('label'); ?></a>
+                          <?php } ?>
+                        <?php endif; ?>
+                    </div>
+                  </div>
                 </div>
               </div>
+              <?php } ?>
             </div>
-          </div>
-          <?php } ?>
         </div>
     <?php endif; ?>
   </div>
