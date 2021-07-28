cd91c84cc415a6cd1967c150e08d451200dd51e3
franei <frank@yora.net>
2021-07-28 23:48:41 +0200

    Add expandable subsections

--- web/app/themes/xrnl/donations.php
+++ web/app/themes/xrnl/donations.php
@@ -10,96 +10,133 @@ get_header(null, array(
 // $fields = get_fields();
 ?>
 
-<style>
-  section:not(:nth-of-type(1)) {
-    margin-top: 3rem;
-  }
-</style>
-
-<section class="hero text-white py-5" style="background: linear-gradient(rgba(0, 0, 0, 0.35), rgba(0, 0, 0, 0.95)), url('https://extinctionrebellion.nl/app/uploads/2021/02/143304662_454786392563007_4345810537040199100_o.jpg') no-repeat center center / cover;">
-  <div class="container-fluid">
-    <div class="row">
-      <div class="content col-12 col-sm-10 col-md-8 col-lg-8 col-xl-7 mx-auto">
-        <h1 class="display-3 text-uppercase font-xr">Steun Extinction Rebellion Nederland</h1>
-        <p>De klimaatcrisis en de massale uitsterving van soorten vereist radicale actie. Jouw bijdrage is nú nodig, harder dan ooit. Alleen met jouw steun kunnen wij de straat op om systeemverandering en sociale rechtvaardigheid te eisen. Doe jij ook mee?</p>
+<div id="donations">
+  <section class="hero text-white py-5" style="background: linear-gradient(rgba(0, 0, 0, 0.35), rgba(0, 0, 0, 0.95)), url('https://extinctionrebellion.nl/app/uploads/2021/02/143304662_454786392563007_4345810537040199100_o.jpg') no-repeat center center / cover;">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="content col-12 col-sm-10 col-md-8 col-lg-8 col-xl-7 mx-auto">
+          <h1 class="display-3 text-uppercase font-xr">Steun Extinction Rebellion Nederland</h1>
+          <p>De klimaatcrisis en de massale uitsterving van soorten vereist radicale actie. Jouw bijdrage is nú nodig, harder dan ooit. Alleen met jouw steun kunnen wij de straat op om systeemverandering en sociale rechtvaardigheid te eisen. Doe jij ook mee?</p>
+        </div>
       </div>
     </div>
-  </div>
-</section>
+  </section>
 
-<section>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="content col-12 col-sm-10 col-md-8 col-lg-8 col-xl-7 mx-auto">
-        <h2>1. Een structurele of eenmalige bijdrage op Whydonate</h2>
-        <p>Je kunt een losse, of maandelijkse gift maken op het crowdfund platform van Whydonate. Houd de pagina ook in de gaten voor specifieke campagnes, zoals tijdens een rebellie, of voor het dekken van juridische kosten na een actie. </p>
-        <div class="row">
-          <div class="mx-auto">
-            <a class="btn btn-lg btn-green" href="https://www.whydonate.nl/fundraising/help-extinction-rebellion-nederland">Doneer</a>
+  <section>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="content col-12 col-sm-10 col-md-8 col-lg-8 col-xl-7 mx-auto">
+          <h2>1. Een structurele of eenmalige bijdrage op Whydonate</h2>
+          <p>Je kunt een losse, of maandelijkse gift maken op het crowdfund platform van Whydonate. Houd de pagina ook in de gaten voor specifieke campagnes, zoals tijdens een rebellie, of voor het dekken van juridische kosten na een actie. </p>
+          <div class="row">
+            <div class="mx-auto">
+              <a class="btn btn-lg btn-green" href="https://www.whydonate.nl/fundraising/help-extinction-rebellion-nederland">Doneer</a>
+            </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="content col-12 col-sm-10 col-md-8 col-lg-8 col-xl-7 mx-auto">
+          <h2>2. Periodiek Schenken met belastingvoordeel</h2>
+          <p>Denk je er aan om Extinction Rebellion Nederland voor een langere tijd te ondersteunen? Overweeg dan om een periodieke schenking te maken. Met periodiek schenken leg je met belastingvoordeel voor 5 jaar een bedrag naar eigen keuze vast. Hierdoor ontvangen wij achter de eindstreep een hogere donatie, terwijl voor jou het bedrag hetzelfde blijft. Zo kunnen wij dus in jou naam nog vaker de straat op!</p>
+          <p>Je kunt een periodieke schenking eenvoudig regelen via <a href="https://www.periodiekschenken.nl/extinction-rebellion">
+            <img src="https://extinctionrebellion.nl/app/uploads/2021/07/periodiekschenken_logo.png" alt="periodiek schenken logo" style="height: 1.2rem; display: inline; margin-left: .2rem;"></a>
+          </p>
+
+          <div class="pl-3">
+            <div class="ps-more-info">
+              <span id="reveal-ps-info" class="ps-info-toggle reveal-ps-info">Waarom periodiek schenken?</span>
+              <span id="hide-ps-info" class="ps-info-toggle hide-ps-info" style="display: none;">Waarom periodiek schenken?</span>
+            </div>
+            <div class="ps-info-text" style="display: none;">
+              <ul>
+                <li>Je geeft meer, zonder dat het jou extra geld kost. En meer inkomsten betekent meer actie!</li>
+                <li>Dankzij jou periodieke schenking hebben wij een stabiele basis van inkomsten en de mogelijkheid om voorruit te plannen.</li>
+                <li>We betalen geen schenkbelasting, dus jou gift kan volledig worden ingezet voor een duurzame en rechtvaardige wereld.</li>
+                <li>Je kunt zelf een bedrag bepalen.</li>
+              </ul>
+            </div>
+            <div class="ps-more-info">
+              <span id="reveal-ps-info" class="ps-info-toggle reveal-ps-info">Praktische zaken</span>
+              <span id="hide-ps-info" class="ps-info-toggle hide-ps-info" style="display: none;">Praktische zaken</span>
+            </div>
+            <div class="ps-info-text" style="display: none;">
+              <ul>
+                <li>De periodieke gift moet worden vastgelegd in een overeenkomst. Deze kan makkelijk geregeld worden via www.periodiekschenken.nl/extinction-rebellion.</li>
+                <li>Er dient minimaal 5 jaar achtereen, ten minste eenmaal per jaar een vast bedrag geschonken te worden.</li>
+                <li>Door tijdsverloop of voortijdig overlijden van de schenker eindigt de verplichting tot betaling.</li>
+                <li>De jaarlijkse schenking is in beginsel geheel aftrekbaar in het jaar dat de betaling wordt gedaan.</li>
+              </ul>
+            </div>
+            <div class="ps-more-info">
+              <span id="reveal-ps-why" class="ps-info-toggle reveal-ps-info">Meer weten?</span>
+              <span id="hide-ps-why" class="ps-info-toggle hide-ps-info" style="display: none;">Meer weten?</span>
+            </div>
+            <div class="ps-info-text" style="display: none;">
+              <ul>
+                <li>Lees de <a href="https://extinctionrebellion.nl/app/uploads/2021/01/schenken-informatie-over-periodiek-schenken.pdf">aanvullende informatie over periodiek schenken (PDF)</a></li>
+                <li>Of neem contact op via <a href="mailto:fondsenwerving@extinctionrebellion.nl">fondsenwerving@extinctionrebellion.nl</a></li>
+              </ul>
+            </div>
           </div>
-          </div>
-      </div>
-    </div>
-  </div>
-</section>
 
-<section>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="content col-12 col-sm-10 col-md-8 col-lg-8 col-xl-7 mx-auto">
-        <h2>2. Periodiek Schenken met belastingvoordeel</h2>
-        <p>Denk je er aan om Extinction Rebellion Nederland voor een langere tijd te ondersteunen? Overweeg dan om een periodieke schenking te maken. Met periodiek schenken leg je met belastingvoordeel voor 5 jaar een bedrag naar eigen keuze vast. Hierdoor ontvangen wij achter de eindstreep een hogere donatie, terwijl voor jou het bedrag hetzelfde blijft. Zo kunnen wij dus in jou naam nog vaker de straat op!</p>
-        <p>Je kunt een periodieke schenking eenvoudig regelen via <a href="https://www.periodiekschenken.nl/extinction-rebellion">
-          <img src="https://extinctionrebellion.nl/app/uploads/2021/07/periodiekschenken_logo.png" alt="periodiek schenken logo" style="height: 1.2rem; display: inline; margin-left: .2rem;"></a>
-        </p>
-        <h3>Waarom periodiek schenken?</h3>
-        <ul>
-          <li>Je geeft meer, zonder dat het jou extra geld kost. En meer inkomsten betekent meer actie!</li>
-          <li>Dankzij jou periodieke schenking hebben wij een stabiele basis van inkomsten en de mogelijkheid om voorruit te plannen.</li>
-          <li>We betalen geen schenkbelasting, dus jou gift kan volledig worden ingezet voor een duurzame en rechtvaardige wereld.</li>
-          <li>Je kunt zelf een bedrag bepalen.</li>
-        </ul>
-        <h3>Praktische zaken</h3>
-        <ul>
-          <li>De periodieke gift moet worden vastgelegd in een overeenkomst. Deze kan makkelijk geregeld worden via www.periodiekschenken.nl/extinction-rebellion.</li>
-          <li>Er dient minimaal 5 jaar achtereen, ten minste eenmaal per jaar een vast bedrag geschonken te worden.</li>
-          <li>Door tijdsverloop of voortijdig overlijden van de schenker eindigt de verplichting tot betaling.</li>
-          <li>De jaarlijkse schenking is in beginsel geheel aftrekbaar in het jaar dat de betaling wordt gedaan.</li>
-        </ul>
-        <h3>Meer weten?</h3>
-        <ul>
-          <li><a href="https://extinctionrebellion.nl/app/uploads/2021/01/schenken-informatie-over-periodiek-schenken.pdf">Aanvullende informatie over periodiek schenken (PDF)</a></li>
-          <li>Of neem contact op via <a href="mailto:fondsenwerving@extinctionrebellion.nl">fondsenwerving@extinctionrebellion.nl</a></li>
-        </ul>
+        </div>
       </div>
     </div>
-  </div>
-</section>
+  </section>
 
-<section>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="content col-12 col-sm-10 col-md-8 col-lg-8 col-xl-7 mx-auto">
-        <h2>3. Een direct gift op het rekeningnummer van Extinction Rebellion NL</h2>
-        <p>Je kunt natuurlijk ook direct een bedrag overboeken op het rekeningnummer van Extinction Rebellion Nederland. Wij zijn ontzettend dankbaar voor alle bijdragen vanuit onze rebelse achterban. Houd wel in gedachten dat structurele en periodieke donaties voor ons inzichtelijker zijn en daarmee efficiënter te besteden aan onze doelstellingen. Toch liever een eenmalige bijdrage direct op het rekening nummer? Je kunt een bedrag overmaken op</p>
-        <p>NL86 SNSB 0783 1486 74 t.n.v. Stichting Vrienden van XR</p>
+  <section>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="content col-12 col-sm-10 col-md-8 col-lg-8 col-xl-7 mx-auto">
+          <h2>3. Een direct gift op het rekeningnummer van Extinction Rebellion NL</h2>
+          <p>Je kunt natuurlijk ook direct een bedrag overboeken op het rekeningnummer van Extinction Rebellion Nederland. Wij zijn ontzettend dankbaar voor alle bijdragen vanuit onze rebelse achterban. Houd wel in gedachten dat structurele en periodieke donaties voor ons inzichtelijker zijn en daarmee efficiënter te besteden aan onze doelstellingen. Toch liever een eenmalige bijdrage direct op het rekening nummer? Je kunt een bedrag overmaken op</p>
+          <p>NL86 SNSB 0783 1486 74 t.n.v. Stichting Vrienden van XR</p>
+        </div>
       </div>
     </div>
-  </div>
-</section>
+  </section>
 
-<section>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="content col-12 col-sm-10 col-md-8 col-lg-8 col-xl-7 mx-auto text-center">
-        <h2>Waar geven wij jou bijdrage aan uit?</h2>
-        <img src="https://extinctionrebellion.nl/app/uploads/2021/07/uitgaven_2020.jpg" alt="XR Nederland Uitgaven 2020">
-        <p class="text-center">
-          <a href="https://extinctionrebellion.nl/stichting-vrienden-van-xr/">Meer weten over inkomsten en uitgaven van Extinction Rebellion NL?</a>
-        </p>
+  <section>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="content col-12 col-sm-10 col-md-8 col-lg-8 col-xl-7 mx-auto text-center">
+          <h2>Waar geven wij jou bijdrage aan uit?</h2>
+          <img src="https://extinctionrebellion.nl/app/uploads/2021/07/uitgaven_2020.jpg" alt="XR Nederland Uitgaven 2020">
+          <p class="text-center">
+            <a href="https://extinctionrebellion.nl/stichting-vrienden-van-xr/">Meer weten over inkomsten en uitgaven van Extinction Rebellion NL?</a>
+          </p>
+        </div>
       </div>
     </div>
-  </div>
-</section>
+  </section>
+
+</div>
+
+<script type="text/javascript">
+   jQuery(document).ready(function() {
+     jQuery('.ps-info-toggle').click(function(e) {
+       jQuery(this).toggle();
+       jQuery(this).siblings('.ps-info-toggle').toggle();
+       jQuery(this).parent().next('.ps-info-text').slideToggle(300);
+       e.preventDefault();
+     });
+   });
+
+
+  // jQuery(document).ready(function() {
+  //    jQuery('.ps-why-toggle').click(function(e) {
+  //      jQuery('.ps-why-toggle').toggle();
+  //      jQuery('#ps-why-text').slideToggle(300);
+  //      e.preventDefault();
+  //    });
+  //  });
+</script>
 
 <?php get_footer(); ?>
+
