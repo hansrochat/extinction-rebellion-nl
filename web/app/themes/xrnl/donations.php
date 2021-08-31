a12a56492a8d0150b765c074caf5c09634f6ae73
franei <frank@yora.net>
2021-08-31 10:11:13 +0200

    Add expenses chart and QR code

--- web/app/themes/xrnl/donations.php
+++ web/app/themes/xrnl/donations.php
@@ -11,7 +11,7 @@ get_header(null, array(
 ?>
 
 <div id="donations">
-  <section class="hero text-white py-5" style="background: linear-gradient(rgba(0, 0, 0, 0.35), rgba(0, 0, 0, 0.95)), url('https://extinctionrebellion.nl/app/uploads/2021/02/143304662_454786392563007_4345810537040199100_o.jpg') no-repeat center center / cover;">
+  <section class="hero text-white py-5" style="background: linear-gradient(rgba(0, 0, 0, 0.35), rgba(0, 0, 0, 0.95)), url('https://extinctionrebellion.nl/app/uploads/2021/08/XR-09042021-ZeldaBonnet-27@0.5x.png') no-repeat center center / cover;">
     <div class="container-fluid">
       <div class="row">
         <div class="content col-12 col-sm-10 col-md-8 col-lg-8 col-xl-7 mx-auto">
@@ -28,9 +28,9 @@ get_header(null, array(
         <div class="content col-12 col-sm-10 col-md-8 col-lg-8 col-xl-7 mx-auto">
           <h2>1. Een structurele of eenmalige bijdrage op Whydonate</h2>
           <p>Je kunt een losse, of maandelijkse gift maken op het crowdfund platform van Whydonate. Houd de pagina ook in de gaten voor specifieke campagnes, zoals tijdens een rebellie, of voor het dekken van juridische kosten na een actie. </p>
-          <div class="row">
+          <div class="row pt-3">
             <div class="mx-auto">
-              <a class="btn btn-lg btn-green" href="https://www.whydonate.nl/fundraising/help-extinction-rebellion-nederland">Doneer</a>
+              <a class="btn btn-lg btn-green" href="https://www.whydonate.nl/fundraising/help-extinction-rebellion-nederland">Doneer via crowdfunding</a>
             </div>
             </div>
         </div>
@@ -44,11 +44,13 @@ get_header(null, array(
         <div class="content col-12 col-sm-10 col-md-8 col-lg-8 col-xl-7 mx-auto">
           <h2>2. Periodiek Schenken met belastingvoordeel</h2>
           <p>Denk je er aan om Extinction Rebellion Nederland voor een langere tijd te ondersteunen? Overweeg dan om een periodieke schenking te maken. Met periodiek schenken leg je met belastingvoordeel voor 5 jaar een bedrag naar eigen keuze vast. Hierdoor ontvangen wij achter de eindstreep een hogere donatie, terwijl voor jou het bedrag hetzelfde blijft. Zo kunnen wij dus in jou naam nog vaker de straat op!</p>
-          <p>Je kunt een periodieke schenking eenvoudig regelen via <a href="https://www.periodiekschenken.nl/extinction-rebellion">
+          <!-- <p>Je kunt een periodieke schenking eenvoudig regelen via <a href="https://www.periodiekschenken.nl/extinction-rebellion">
             <img src="https://extinctionrebellion.nl/app/uploads/2021/07/periodiekschenken_logo.png" alt="periodiek schenken logo" style="height: 1.2rem; display: inline; margin-left: .2rem;"></a>
+          </p> -->
+          <p>Je kunt een periodieke schenking eenvoudig regelen via <a href="https://www.periodiekschenken.nl/extinction-rebellion">periodiekschenken.nl</a>
           </p>
 
-          <div class="pl-3">
+          <div class="pl-3 mt-4">
             <div class="ps-more-info">
               <span id="reveal-ps-info" class="ps-info-toggle reveal-ps-info">Waarom periodiek schenken?</span>
               <span id="hide-ps-info" class="ps-info-toggle hide-ps-info" style="display: none;">Waarom periodiek schenken?</span>
@@ -96,20 +98,129 @@ get_header(null, array(
         <div class="content col-12 col-sm-10 col-md-8 col-lg-8 col-xl-7 mx-auto">
           <h2>3. Een direct gift op het rekeningnummer van Extinction Rebellion NL</h2>
           <p>Je kunt natuurlijk ook direct een bedrag overboeken op het rekeningnummer van Extinction Rebellion Nederland. Wij zijn ontzettend dankbaar voor alle bijdragen vanuit onze rebelse achterban. Houd wel in gedachten dat structurele en periodieke donaties voor ons inzichtelijker zijn en daarmee efficiënter te besteden aan onze doelstellingen. Toch liever een eenmalige bijdrage direct op het rekening nummer? Je kunt een bedrag overmaken op</p>
-          <p>NL86 SNSB 0783 1486 74 t.n.v. Stichting Vrienden van XR</p>
+          <p class="text-center font-weight-bold">NL86SNSB0783148674 <em>t.n.v.</em> Stichting Vrienden van XR</p>
+          <div class="pl-3 mt-4">
+            <div class="ps-more-info">
+                <span id="reveal-ps-why" class="ps-info-toggle reveal-ps-info">Scan QR code</span>
+                <span id="hide-ps-why" class="ps-info-toggle hide-ps-info" style="display: none;">Verberg QR code</span>
+            </div>
+              <div class="ps-info-text" style="display: none;">
+                <div class=row>
+                <div class="col-10 col-md-8 col-lg-5 col-xl-4 mx-auto pt-4">
+                  <img src="https://extinctionrebellion.nl/app/uploads/2021/08/qr-code.png" alt="Stichting Vrienden van XR SEPA link">
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </section>
 
+  <?php  
+    $cdata = array(
+      array(
+        'label' => 'Actiekosten',
+        'short_label' => 'Acties',
+        'value' => 42
+      ),
+      array(
+        'label' => 'Promotiekosten',
+        'short_label' => 'Promotie',
+        'value' => 15
+      ),
+      array(
+        'label' => 'Juridische kosten',
+        'short_label' => 'Juridisch',
+        'note' => '(geen boetes)',
+        'value' => 12
+      ),
+      array(
+        'label' => 'Trainingskosten',
+        'short_label' => 'Training',
+        'note' => '(NVDA & Talks)',
+        'value' => 8
+      ),
+      array(
+        'label' => 'Global support',
+        'note' => '(Steun voor XR groepen wereldwijd)',
+        'value' => 8
+      ),
+      array(
+        'label' => 'Wervingskosten',
+        'short_label' => 'Werving',
+        'note' => '',
+        'value' => 6
+      ),
+      array(
+        'label' => 'Huurkosten actieruimten',
+        'short_label' => 'Actieruimten',
+        'note' => '',
+        'value' => 5
+      ),
+      array(
+        'label' => 'Automatiseringskosten',
+        'short_label' => 'Automatisering',
+        'note' => 'en div.',
+        'value' => 3
+      ),
+      array(
+        'label' => 'Administratiekosten',
+        'short_label' => 'Administratie',
+        'note' => '',
+        'value' => 1
+      )
+
+    );
+    // Get the largest value of all data points
+    function xrnl_get_max_dp($arr) {
+      $max=0;
+      foreach ($arr as $dp) {
+        if ($dp['value'] > $max) {
+          $max = $dp['value'];
+        }
+      }
+      return $max;
+    }
+    $max_dp = xrnl_get_max_dp($cdata);
+  ?>
   <section>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="content col-12 col-sm-10 col-md-8 col-lg-8 col-xl-7 mx-auto text-center">
-          <h2>Waar geven wij jou bijdrage aan uit?</h2>
-          <img src="https://extinctionrebellion.nl/app/uploads/2021/07/uitgaven_2020.jpg" alt="XR Nederland Uitgaven 2020">
-          <p class="text-center">
-            <a href="https://extinctionrebellion.nl/stichting-vrienden-van-xr/">Meer weten over inkomsten en uitgaven van Extinction Rebellion NL?</a>
+    <div class="container-fluid bg-xr-lemon pt-4">
+      <div class="expenses-chart row">
+        <div class="content col-12 col-sm-10 col-md-8 col-lg-7 col-xl-7 mx-auto">
+          <h2 class="mb-2 mb-md-1">Waar geven wij jou bijdrage aan uit?</h2>
+          <div class="selected-dp-info row">
+            <div class="col-10">
+              <span class="dp-info-item dp-info-label"></span>
+              <span class="dp-info-item dp-info-note pr-4"></span>
+            </div>
+            <span class="dp-info-item dp-info-value text-right font-xr col-2"></span>
+          </div>
+            <?php
+              $elcount = 0;
+              foreach($cdata as $datapoint) :
+                $elcount++;
+            ?>
+            <div class="chart-line row"
+              data-label  = "<?php echo $datapoint['label']; ?>"
+              data-note   = "<?php echo $datapoint['note'] ?: ''; ?>"
+              data-value  = "<?php echo $datapoint['value']; ?>"
+            >
+              <div class="dp-label line-segment col-6 col-sm-5 col-md-5 col-lg-4 text-right">
+                  <?php echo $datapoint['short_label'] ?: $datapoint['label'] ?>
+              </div>
+              <div class="line-segment col-4 col-sm-5 col-md-6 col-lg-7">
+                <div class="dp-bar" style="width: <?php echo ($datapoint['value'] / $max_dp) * 100 ?>%">
+                </div>
+              </div>
+              <div class="dp-value line-segment col-1 text-right">
+                <?php echo $datapoint['value']; ?>%
+              </div>
+            </div>
+          <?php endforeach; ?>
+
+          <p class="text-center mt-5">
+            Meer weten over inkomsten en uitgaven van Extinction Rebellion NL?<br><a href="https://extinctionrebellion.nl/stichting-vrienden-van-xr/#financiele-verantwoording">Kijk hier</a>
           </p>
         </div>
       </div>
@@ -120,23 +231,24 @@ get_header(null, array(
 
 <script type="text/javascript">
    jQuery(document).ready(function() {
-     jQuery('.ps-info-toggle').click(function(e) {
-       jQuery(this).toggle();
-       jQuery(this).siblings('.ps-info-toggle').toggle();
-       jQuery(this).parent().next('.ps-info-text').slideToggle(300);
-       e.preventDefault();
-     });
-   });
-
-
-  // jQuery(document).ready(function() {
-  //    jQuery('.ps-why-toggle').click(function(e) {
-  //      jQuery('.ps-why-toggle').toggle();
-  //      jQuery('#ps-why-text').slideToggle(300);
-  //      e.preventDefault();
-  //    });
-  //  });
+      // Expand section toggles
+      jQuery('.ps-info-toggle').click(function(e) {
+        jQuery(this).toggle();
+        jQuery(this).siblings('.ps-info-toggle').toggle();
+        jQuery(this).parent().next('.ps-info-text').slideToggle(300);
+        e.preventDefault();
+      });
+      // Show selected chart info on hover
+      jQuery('.chart-line').hover(function() {
+        jQuery('.dp-info-label').text(jQuery(this).attr('data-label'));
+        jQuery('.dp-info-note').text(jQuery(this).attr('data-note'));
+        jQuery('.dp-info-value').text(jQuery(this).attr('data-value')+'%');
+        jQuery('.selected-dp-info').addClass('active');
+      }, function() {
+        jQuery('.dp-info-item').text('');
+        jQuery('.selected-dp-info').removeClass('active');
+      });
+    });
 </script>
 
 <?php get_footer(); ?>
-
