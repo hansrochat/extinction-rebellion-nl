298aa43a17d9d35592c15f252cf354a5e3eba6d8
Jur de Vries <info@jurdevries.nl>
2021-10-07 14:21:50 +0200

    Created basic design for new join page.

---
+++ web/app/themes/xrnl/join_new.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Template name: Join New
+ *
+ * Join page overview
+ */
+
+get_header(); ?>
+
+<div class="join">
+    <div class="bg-xr-white">
+        <div class="bg-xr-warning mx-2 my-2 py-5 px-5">
+            <div class="font-weight-bold text-uppercase font-xr">We have a moral duty to rebel. whatever our politics.</div>
+            <h1 class="text-light join-title">Take action now.</h1>
+            <p>Our strategy is one of non-violent, disruptive civil disobedience – a rebellion. We need you – whoever
+                you
+                are, however much time you have to offer. Together we're building a powerful movement that is
+                participatory
+                and inclusive. Togethr we're unstoppable.</p>
+        </div>
+        <div class="bg-xr-info mx-2 my-2 py-5 px-5">
+            <div class="font-weight-bold text-uppercase font-xr">Time to make us unstoppable.</div>
+            <h2 class="text-light join-subtitle">Join the rebellion!</h2>
+
+            <div class="join-first-row">
+                <div class="px-4 py-4 mx-2 my-2 join-row-element">
+                    <h3 class="text-uppercase text-info font-xr">01. Sign up.</h3>
+                    <p>Sign up to the XR newsletter, and keep up to date with the latest news from all over the world</p>
+                    <a class="join-row-element__cta btn btn-black btn-lg" href="#">Sign up to mailing list</a>
+                </div>
+
+                <div class="px-4 py-4 mx-2 my-2 join-row-element">
+                    <h3 class="text-uppercase text-info font-xr">02. Meet up.</h3>
+                    <p>XR takes place both online and offline. Find local rebels,  meet up and take action.</p>
+                    <a class="join-row-element__cta btn btn btn-black btn-lg" href="#">Find your group</a>
+                </div>
+            </div>
+
+            <div class="join-second-row">
+                <div class="px-4 py-4 mx-2 my-2 join-row-element">
+                    <h3 class="text-uppercase text-info font-xr">03. Train Up</h3>
+                    <p>What is XR all  about? Attend an intro talk to find out more about XR’s core strategy and how to get
+                        involved.</p>
+                    <a class="join-row-element__cta btn btn btn-black btn-lg" href="#">watch an introductory talk</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
