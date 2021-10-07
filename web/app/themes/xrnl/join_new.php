628b68541ffc8bd78e58c9798bed7368a3082932
Jur de Vries <info@jurdevries.nl>
2021-10-07 17:25:20 +0200

    Added all the fields for the new join page.

--- web/app/themes/xrnl/join_new.php
+++ web/app/themes/xrnl/join_new.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template name: Join New
+ * Template name: Join page overview
  *
  * Join page overview
  */
@@ -11,7 +11,7 @@ get_header(); ?>
     <div class="bg-xr-white">
         <div class="bg-xr-warning mx-2 my-2 join-top">
             <div class="join-top__image">
-                <img src="/app/uploads/2020/09/Bitmap@2x-scaled.jpg" alt="image">
+                <img src="<?php the_field('top_image'); ?>" alt="image">
             </div>
             <div class="join-top__text">
                 <svg viewBox="0 0 50 100" preserveAspectRatio="none" class="join-top__text__svg">
@@ -19,40 +19,39 @@ get_header(); ?>
                     <polygon fill="" points="0,0 5,0 50,100 45,100" class="join-top__text__line"></polygon>
                 </svg>
                 <div class="py-5 px-5">
-                    <div class="font-weight-bold text-uppercase font-xr">We have a moral duty to rebel. whatever our politics.</div>
-                    <h1 class="text-light join-title">Take action now.</h1>
-                    <p>Our strategy is one of non-violent, disruptive civil disobedience – a rebellion. We need you – whoever
-                        you
-                        are, however much time you have to offer. Together we're building a powerful movement that is
-                        participatory
-                        and inclusive. Togethr we're unstoppable .,...</p>
+                    <div class="font-weight-bold text-uppercase font-xr"><?php the_field('top_subtitle'); ?></div>
+                    <h1 class="text-light join-title"><?php the_field('top_title'); ?></h1>
+                    <p><?php the_field('top_text'); ?></p>
                 </div>
             </div>
         </div>
         <div class="bg-xr-info mx-2 my-2 py-5 px-5">
-            <div class="font-weight-bold text-uppercase font-xr">Time to make us unstoppable.</div>
-            <h2 class="text-light join-subtitle">Join the rebellion!</h2>
+            <div class="font-weight-bold text-uppercase font-xr"><?php the_field('content_subtitle'); ?></div>
+            <h2 class="text-light join-subtitle"><?php the_title(); ?></h2>
 
             <div class="join-first-row">
+	            <?php $section1 = getSection('sign_up'); ?>
                 <div class="px-4 py-4 mx-2 my-2 join-row-element">
-                    <h3 class="text-uppercase text-info font-xr">01. Sign up.</h3>
-                    <p>Sign up to the XR newsletter, and keep up to date with the latest news from all over the world</p>
-                    <a class="join-row-element__cta btn btn-black btn-lg" href="#">Sign up to mailing list</a>
+                    <h3 class="text-uppercase text-info font-xr"><?php echo ($section1->title); ?></h3>
+                    <p><?php echo ($section1->text); ?></p>
+                    <a class="join-row-element__cta btn btn-black btn-lg" href="<?php echo ($section1->cta_link); ?>"><?php echo ($section1->cta_text); ?></a>
                 </div>
 
+	            <?php $section2 = getSection('meet_up'); ?>
                 <div class="px-4 py-4 mx-2 my-2 join-row-element">
-                    <h3 class="text-uppercase text-info font-xr">02. Meet up.</h3>
-                    <p>XR takes place both online and offline. Find local rebels,  meet up and take action.</p>
-                    <a class="join-row-element__cta btn btn btn-black btn-lg" href="#">Find your group</a>
+                    <h3 class="text-uppercase text-info font-xr"><?php echo ($section2->title); ?></h3>
+                    <p><?php echo ($section2->text); ?></p>
+                    <a class="join-row-element__cta btn btn-black btn-lg" href="<?php echo ($section2->cta_link); ?>"><?php echo ($section1->cta_text); ?></a>
                 </div>
             </div>
 
             <div class="join-second-row">
+	            <?php $section3 = getSection('train_up'); ?>
                 <div class="px-4 py-4 mx-2 my-2 join-row-element">
-                    <h3 class="text-uppercase text-info font-xr">03. Train Up</h3>
-                    <p>What is XR all  about? Attend an intro talk to find out more about XR’s core strategy and how to get
-                        involved.</p>
-                    <a class="join-row-element__cta btn btn btn-black btn-lg" href="#">watch an introductory talk</a>
+                    <h3 class="text-uppercase text-info font-xr"><?php echo ($section3->title); ?></h3>
+                    <p><?php echo ($section3->text); ?></p>
+                    <a class="join-row-element__cta btn btn-black btn-lg" href="<?php echo ($section3->cta_link); ?>"><?php echo ($section1->cta_text); ?></a>
+
                 </div>
             </div>
         </div>
