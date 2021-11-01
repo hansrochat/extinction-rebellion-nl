145b25dd1abd39a0f194631fce9c1e6d0ca6b651
franei <frank@yora.net>
2021-11-01 22:44:16 +0100

    Register smooth-scrolling javascript

---
+++ web/app/themes/xrnl/assets/js/smooth-scrolling.js
@@ -0,0 +1,33 @@
+jQuery(document).ready(function($) {
+
+  // Accessible smooth scrolling. Credits: Chris Coyier
+  // https://css-tricks.com/snippets/jquery/smooth-scrolling/
+  
+  $('a[href*="#"]').click(function(event) {
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500, function() {
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) {
+            return false;
+          } else {
+            $target.attr('tabindex','-1');
+            $target.focus();
+          };
+        });
+      }
+    }
+  });
+
+});
+  
